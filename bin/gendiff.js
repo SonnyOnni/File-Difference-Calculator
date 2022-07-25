c0a88a7666e98b8a445e92481e2e479b4458b19c
Sonny <li.zero@yandex.ru>
2022-07-25 21:12:30 +0300

    finished 3 step of project

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -1,9 +1,16 @@
 #!/usr/bin/env node
 import { Command } from 'commander/esm.mjs';
+import getDiff from '../src/index.js';
+
 const program = new Command();
 
 program
   .description('Compares two configuration files and shows a difference.')
   .version('0.8.0')
+  .option('-f, --format <type>', 'output format')
+  .argument('<filepath1> <filepath2>')
+  .action((filepath1, filepath2) => {
+    console.log(getDiff(filepath1, filepath2));
+  });
 
 program.parse();
---
+++ src/index.js
@@ -0,0 +1,68 @@
+import _ from "lodash";
+import fs from 'fs';
+
+const getDiff = (file1, file2) => {
+  const objFile1 = fs.readFileSync('./file1.json', 'utf-8');
+  const objFile2 = fs.readFileSync('./file2.json', 'utf-8');
+
+  const parsedFile1 = JSON.parse(objFile1);
+  const parsedFile2 = JSON.parse(objFile2);
+
+  const keys1 = Object.keys(parsedFile1);
+  const keys2 = Object.keys(parsedFile2);
+  const uniteKeys = _.sortBy(_.union(keys1, keys2));
+
+  const changingKeys = uniteKeys.map((key) => {
+    if(!_.has(parsedFile1, key)) {
+      return {
+        name: key,
+        value: parsedFile2[key],
+        type: 'added',
+      }
+    } 
+    if(!_.has(parsedFile2, key)) {
+      return {
+        name: key,
+        value: parsedFile1[key],
+        type: 'deleted',
+      }
+    } 
+    if(parsedFile1[key] !== parsedFile2[key]) {
+      return {
+        name: key,
+        value1: parsedFile1[key],
+        value2: parsedFile2[key],
+        type: 'changed',
+      }
+    }
+    return {
+      name: key,
+      value: parsedFile1[key],
+      type: 'unchanged',
+    }
+  })
+
+  const anotherResult = changingKeys.reduce((acc, item) => {
+    if(item.type === 'unchanged') {
+      acc += `   ${item.name}: ${item.value}\n`;
+      return acc;
+    }
+    if(item.type === 'deleted') {
+      acc += ` - ${item.name}: ${item.value}\n`;
+      return acc;
+    }
+    if(item.type === 'added') {
+      acc += ` + ${item.name}: ${item.value}\n`;
+      return acc;
+    }
+    if(item.type === 'changed') {
+      acc += ` - ${item.name}: ${item.value1}\n`;
+      acc += ` + ${item.name}: ${item.value2}\n`;
+      return acc;
+    }
+  }, '')
+
+  return `{\n${anotherResult}}`;
+};
+
+export default getDiff;
